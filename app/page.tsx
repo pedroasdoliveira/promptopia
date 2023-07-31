5020036cfdc2fdc7301dbc27d309ec016217cc1d
Pedro Oliveira <Pedroaolive@gmail.com>
2023-07-31 18:03:58 -0300

    feat: add changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
-}
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Descubra & compartilhe
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center text-4xl">
+          Prompts alimentados por IA
+        </span>
+      </h1>
+
+      <p className="desc text-center">
+        A Promptopia é uma ferramenta de código aberto para o mundo moderno
+        descobrir, criar e partilhar sugestões criativas
+          </p>
+          
+          {/* Feed */}
+    </section>
+  );
+};
 
 export default Home;
